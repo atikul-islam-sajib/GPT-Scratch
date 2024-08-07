b8b38fec332c8dcd5c170e4908f0d5fe4f43a4ac
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-08-08 01:10:08 +0200

    Update setup.py file initially and will be updated later

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ViT",
+    name="tinyGPT",
     version="0.0.1",
-    description="A deep learning project for Transformer-based tasks using ViT",
+    description="A deep learning project for Transformer-based tasks using GPT",
     author="Atikul Islam Sajib",
     author_email="atikulislamsajib137@gmail.com",
-    url="https://github.com/atikul-islam-sajib/ViT-Scratch",
+    url="https://github.com/atikul-islam-sajib/GPT-Scratch",
     packages=find_packages(),
-    install_requires=[
-        "numpy==1.21.2",
-        "pandas==1.3.3",
-        "torch==1.9.0",
-    ],
+    install_requires=[],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -21,8 +17,8 @@ setup(
     ],
     keywords="TransformerScratch, machine-learning",
     project_urls={
-        "Bug Tracker": "https://github.com/atikul-islam-sajib/ViT-Scratch/issues",
-        "Documentation": "https://github.com/atikul-islam-sajib/ViT-Scratch",
-        "Source Code": "https://github.com/atikul-islam-sajib/ViT-Scratch",
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/GPT-Scratch/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/GPT-Scratch",
+        "Source Code": "https://github.com/atikul-islam-sajib/GPT-Scratch",
     },
 )
