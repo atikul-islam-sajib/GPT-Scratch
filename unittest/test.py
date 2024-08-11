084dc9949cdfcdaaaab8f33ea5f9511327eb21e5
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-11 20:27:00 +0200

    scaled dot product passed the unittest

---
+++ unittest/test.py
@@ -0,0 +1,55 @@
+import sys
+import torch
+import unittest
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+from utils import config
+from scaled_dot_product import scaled_dot_product_attention
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.batch_size = config()["embedding"]["batch_size"]
+        self.block_size = config()["embedding"]["block_size"]
+
+        self.dimension = config()["GPT"]["dimension"]
+        self.nheads = config()["GPT"]["nheads"]
+        self.dropout = config()["GPT"]["dropout"]
+        self.bias = config()["GPT"]["bias"]
+
+    def test_scaled_dot_prododuct(self):
+        self.assertEqual(
+            scaled_dot_product_attention(
+                query=torch.randn(
+                    self.batch_size,
+                    self.nheads,
+                    self.block_size,
+                    self.dimension // self.nheads,
+                ),
+                key=torch.randn(
+                    self.batch_size,
+                    self.nheads,
+                    self.block_size,
+                    self.dimension // self.nheads,
+                ),
+                value=torch.randn(
+                    self.batch_size,
+                    self.nheads,
+                    self.block_size,
+                    self.dimension // self.nheads,
+                ),
+                mask=torch.randn(self.batch_size, self.block_size),
+            ).size(),
+            (
+                self.batch_size,
+                self.nheads,
+                self.block_size,
+                self.dimension // self.nheads,
+            ),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
