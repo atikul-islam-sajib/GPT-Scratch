2579c2def370315cee9430d4d90f37cf1637ef16
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-11 20:38:27 +0200

    multihead attention passed the unittest for masking and no mask as well

--- unittest/test.py
+++ unittest/test.py
@@ -2,11 +2,13 @@ import sys
 import torch
 import unittest
 import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
 
 sys.path.append("./src/")
 
 from utils import config
 from scaled_dot_product import scaled_dot_product_attention
+from multihead_attention import MultiHeadAttentionLayer
 
 
 class UnitTest(unittest.TestCase):
@@ -18,6 +20,13 @@ class UnitTest(unittest.TestCase):
         self.nheads = config()["GPT"]["nheads"]
         self.dropout = config()["GPT"]["dropout"]
         self.bias = config()["GPT"]["bias"]
+
+        self.multihead = MultiHeadAttentionLayer(
+            dimension=self.dimension,
+            nheads=self.nheads,
+            dropout=self.dropout,
+            bias=self.bias,
+        )
 
     def test_scaled_dot_prododuct(self):
         self.assertEqual(
@@ -78,6 +87,41 @@ class UnitTest(unittest.TestCase):
                 self.block_size,
                 self.dimension // self.nheads,
             ),
+        )
+
+    def test_multihead_attention_layer(self):
+        X = torch.randint(0, self.block_size, (self.batch_size * 20, self.block_size))
+        y = torch.randint(0, self.block_size, (self.batch_size * 20, self.block_size))
+        mask = torch.randint(0, 2, (self.batch_size * 20, self.block_size))
+
+        self.assertEqual(X.size(), y.size())
+
+        dataset = TensorDataset(X, mask)
+
+        dataloader = DataLoader(
+            dataset=list(zip(dataset, y)),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+        (independent, mask), dependent = next(iter(dataloader))
+
+        self.assertEqual(independent.size(), dependent.size(), mask.size())
+
+        embedding_layer = nn.Embedding(
+            num_embeddings=self.block_size, embedding_dim=self.dimension
+        )
+
+        embedding = embedding_layer(independent)
+
+        self.assertEqual(
+            self.multihead(x=embedding, mask=None).size(),
+            (self.batch_size, self.block_size, self.dimension),
+        )
+
+        self.assertEqual(
+            self.multihead(x=embedding, mask=mask).size(),
+            (self.batch_size, self.block_size, self.dimension),
         )
 
 
