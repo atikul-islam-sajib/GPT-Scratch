f9202545ed2fe3bba36538ac071d6bd09bbd71a8
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-11 19:28:58 +0200

    query, key, and value is calculated

--- src/multihead_attention.py
+++ src/multihead_attention.py
@@ -21,5 +21,38 @@ class MultiHeadAttentionLayer(nn.Module):
 
         self.dimension % self.nheads == 0, "dimension must be divisible by nheads".capitalize()
 
+        self.QKV = nn.Linear(
+            in_features=self.dimension, out_features=3 * self.dimension, bias=self.bias
+        )
+
     def forward(self, x: torch.Tensor, mask=None):
-        pass
+        if isinstance(x, torch.Tensor):
+            QKV = self.QKV(x)
+
+            query, key, value = torch.chunk(input=QKV, chunks=3, dim=-1)
+
+            assert (
+                query.size() == key.size() == value.size()
+            ), "QKV must have the same size".capitalize()
+
+            query = query.view(
+                query.size(0), query.size(1), self.nheads, self.dimension // self.nheads
+            )
+            key = key.view(
+                key.size(0), key.size(1), self.nheads, self.dimension // self.nheads
+            )
+            value = value.view(
+                value.size(0), value.size(1), self.nheads, self.dimension // self.nheads
+            )
+
+            query = query.permute(0, 2, 1, 3)
+            key = key.permute(0, 2, 1, 3)
+            value = value.permute(0, 2, 1, 3)
+
+            return value
+
+
+if __name__ == "__main__":
+    attention = MultiHeadAttentionLayer(dimension=512, nheads=8, dropout=0.1, bias=True)
+
+    print(attention(torch.randn(40, 200, 512)).size())
