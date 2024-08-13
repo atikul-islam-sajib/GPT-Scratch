781d68c28531c983f09d60ae148fa6e34f574b1d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-08-13 20:43:17 +0200

    GPT prototype is added

---
+++ src/GPT.py
@@ -0,0 +1,52 @@
+import sys
+import torch
+import argparse
+import torch.nn as nn
+
+sys.path.append("./src/")
+
+from utils import config
+from transformer import TransformerEncoderBlock
+
+
+class GPT(nn.Module):
+    def __init__(
+        self,
+        dimension: int = 512,
+        nheads: int = 8,
+        num_layers: int = 6,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+        activation: str = "gelu",
+        eps: float = 1e-5,
+        bias: bool = True,
+    ):
+        super(GPT, self).__init__()
+
+        self.dimension = dimension
+        self.nheads = nheads
+        self.num_layers = num_layers
+        self.dim_feedforward = dim_feedforward
+        self.dropout = dropout
+        self.activation = activation
+        self.eps = eps
+        self.bias = bias
+
+        self.model = nn.Sequential(
+            *[
+                TransformerEncoderBlock(
+                    dimension=self.dimension,
+                    nheads=self.nheads,
+                    dim_feedforward=self.dim_feedforward,
+                    dropout=self.dropout,
+                    activation=self.activation,
+                    eps=self.eps,
+                    bias=self.bias,
+                )
+                for _ in range(self.num_layers)
+            ]
+        )
+
+    def forward(self, x: torch.Tensor, mask=None):
+        if isinstance(x, torch.Tensor):
+            pass
